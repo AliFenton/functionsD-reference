605839d300b7a89287653f7d90f8b9c56c31018d
AliFenton <terraafenton@student.hartnell.edu>
2015-10-15 18:59:45 -0700

    uploading code

---
+++ Fenton_reference.cpp
@@ -0,0 +1,65 @@
+// Name: Ali Fenton
+// Date: October 16,2015
+// Programming Practice - Functions D
+
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+void sort(int& numA, int& numB, int& numC); // Problem 1
+void numDigits(int  valA, int& n); // Problem 2
+void computeSphere(double& a, double& v, double r); // Problem 3
+void swap(int& A, int& B); // Problem 4
+
+int main(){
+    // Problem 4
+    int x = 2, y = 3;
+    swap(x,y);
+    assert(x == 3);
+    //assert(y == 2);
+cout << y;
+    return 0;
+}
+
+//*****************************************
+void sort(int& numA, int& numB, int& numC){
+// Summary: Sorts the 3 numbers by increasing order
+// Precondition: The 3 numbers will be between 0-100
+// Postcondition: It will put them in order from smallest to biggest
+
+
+
+}
+
+//*****************************************
+void numDigits(int valA, int& n){
+// Summary: 
+// Precondition: 
+// Postcondition: 
+
+
+
+}
+
+//******************************************
+void computeSphere(double& a, double& v, double r){
+// Summary: 
+// Precondition: 
+// Postcondition: 
+
+
+
+}
+
+//*******************************************
+void swap(int& a, int& b){
+// Summary: 
+// Precondition: 
+// Postcondition: 
+    int result;
+    a = result;
+    a = b;
+    b = result;
+    
+    return;
+}
